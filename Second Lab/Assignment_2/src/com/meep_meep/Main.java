8182a407ba3a90baec66e022b994102ab1523f73
Dean <deancs1997@gmail.com>
2019-03-07 21:27:02 -0500

    Reading File now working

--- Second Lab/Assignment_2/src/com/meep_meep/Main.java
+++ Second Lab/Assignment_2/src/com/meep_meep/Main.java
@@ -7,8 +7,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        final int firstColumn = 0;
-        final int secondColumn = 1;
+        final int firstColumnIndex = 0;
+        final int secondColumnIndex = 1;
         int userCounter = 0;
         ArrayList<User> users = new ArrayList<>();
 
@@ -35,31 +35,47 @@ public class Main {
             inputFile = new File(args[0]);
         }
 
-        try (Scanner readingLine = new Scanner( inputFile)) {
+        try (Scanner readingLine = new Scanner(inputFile)) {
             int quantumTime = Integer.parseInt(readingLine.nextLine());
 
             Scheduler scheduler = new Scheduler(quantumTime);
+            System.out.println("Quantum time is " + quantumTime);
+            String[] lineReadArray;
 
-            String[] lineRead;
+            while (readingLine.hasNextLine()) {
+                String fullLine = readingLine.nextLine();
+                lineReadArray = fullLine.split("\\s", 2);
+                String first = lineReadArray[firstColumnIndex].trim();
 
-            while(readingLine.hasNextLine()){
+                System.out.println(fullLine);
+//                System.out.println("First is "+ first);
 
-                lineRead = readingLine.nextLine().split("\t");
+                if (isAlpha(first)) {
 
-                if(isAlpha(lineRead[firstColumn].trim())){
+                    String userName = lineReadArray[firstColumnIndex].trim();
+                    int numOfProcesses = Integer.parseInt(lineReadArray[secondColumnIndex].trim());
+                    users.add(new User(userName, numOfProcesses));
 
-                String userName = lineRead[firstColumn].trim();
-                int numOfProcesses = Integer.parseInt(lineRead[secondColumn]);
-                users.add(new User(userName, numOfProcesses));
+                    System.out.println("here");
+                    System.out.println("User: " + userName + " number of Processes: " + numOfProcesses);
 
-                while (numOfProcesses != 0){
+                    Process processes[] = new Process[numOfProcesses];
+                    int index = 0;
+                    while (index < numOfProcesses) {
+                        fullLine = readingLine.nextLine();
+                        System.out.println(fullLine);
+                        lineReadArray = fullLine.split("\\s", 2);
+                        int startTime = Integer.parseInt(lineReadArray[firstColumnIndex].trim());
+                        int duration = Integer.parseInt(lineReadArray[secondColumnIndex].trim());
 
+                        System.out.println("Starting: " + startTime + " duration: " + duration);
+                        processes[index] = new Process(users.get(userCounter), index, startTime, duration);
+                        index++;
+                    }
+                    users.get(userCounter).setProcesses(processes);
+                    System.out.println(userCounter + 1 +  " users created");
+                    userCounter++;
                 }
-
-                }else {
-                    users[userCounter].setPr
-                }
-
             }
 
 
@@ -69,11 +85,14 @@ public class Main {
     }
 
 
+    /*
+     * Function which checks if a string does not contain numbers*/
+
     private static boolean isAlpha(String name) {
         char[] chars = name.toCharArray();
 
         for (char c : chars) {
-            if(!Character.isLetter(c)) {
+            if (!Character.isLetter(c)) {
                 return false;
             }
         }

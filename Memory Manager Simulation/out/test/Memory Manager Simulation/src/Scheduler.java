42ee9e24195f81594f2cca9fbdb7f3e1f4710932
Dean <deancs1997@gmail.com>
2019-03-30 17:48:27 -0400

    Renamed Project Name for GitHub
    Fixed a bug which happened when adding processes to an uninitialized List

--- Memory Manager Simulation/out/test/Memory Manager Simulation/src/Scheduler.java
+++ Memory Manager Simulation/out/test/Memory Manager Simulation/src/Scheduler.java
@@ -1,15 +1,17 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 
-public class Scheduler implements Runnable{
+public class Scheduler implements Runnable {
     CommandManager commandManager;
-    List<Process> processes;
+    List<Process> processes = new ArrayList<Process>();
+
     @Override
     public void run() {
         setup();
 
-        for(int time = 0; true; time+=50){
+        for (int time = 0; true; time += 50) {
 
             notify(time);
 
@@ -21,20 +23,20 @@ public class Scheduler implements Runnable{
         }
     }
 
-    private void setup(){
+    private void setup() {
         try {
             CommandManager.setup(new File("commands.txt"));
             MemoryManager.setup(new File("memoryConfig.txt"), new File("disk.txt"));
-        }catch (FileNotFoundException ex){
+        } catch (FileNotFoundException ex) {
             System.out.println(ex.getMessage());
         }
     }
 
-    public void addProcess(Process p){
+    public void addProcess(Process p) {
         processes.add(p);
     }
 
-    private void notify(int time){
+    private void notify(int time) {
         processes.forEach(process -> process.check(time));
     }
 }
--- Memory Manager Simulation/src/Main.java
+++ Memory Manager Simulation/src/Main.java
@@ -6,22 +6,22 @@ public class Main {
     public static void main(String[] args) {
         try {
             Scheduler scheduler = new Scheduler();
-            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("./Testing/output.txt"));
 
-            File file = new File("processes.txt");
+            File file = new File("./Testing/processes.txt");
             Scanner input = new Scanner(file);
 
             //Discard Number as we dont care;
             input.nextLine();
 
-            for(int i = 1; input.hasNext(); i++){
+            for (int i = 1; input.hasNextLine(); i++) {
                 String[] temp = input.nextLine().split(" ");
-                scheduler.addProcess(new Process(i, Integer.parseInt(temp[0]), Integer.parseInt(temp[1]),writer));
+                scheduler.addProcess(new Process(i, Integer.parseInt(temp[0]), Integer.parseInt(temp[1]), writer));
             }
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        }catch (IOException ex){
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
